4dce5b82e93162f29185c3c2ccb5a85ca7df0cf1
inori_lover <inori_lover@outlook.com>
2024-01-16 16:08:10 +0800

    fix: wrong type declera in querySelector

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 import { setupHearts }from'./snippets/hearts/index.ts'
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
-setupHearts(document.querySelector<HTMLButtonElement>('#hearts')!)
+setupHearts(document.querySelector<HTMLSpanElement>('#hearts')!)
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
