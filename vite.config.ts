00b64156d6bcc3e09fb9a0801ed4287608014d98
Hank <hank-c@qq.com>
2024-01-16 11:00:15 +0800

    chore: build base ''

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // 此处改为你项目地址
-  base: "/template-vite-ts/",
+  base: "",
   build: {
     outDir: "docs",
-    
   }
 })
+    
